94b19c0e0c35d50fbda9f640b490feb8dd5f362a
Lowis <raptorcreator12@gmail.com>
2023-08-21 10:00:06 +0200

    Content of the repository adapted to the fork

--- .gitlab/scripts/security.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# RUSTSEC-2021-0119: out-of-bounds write in nix::unistd::getgrouplist in a old nix version (0.18 and 0.20) that are needed by old winit,
-time cargo audit --ignore RUSTSEC-2021-0119
